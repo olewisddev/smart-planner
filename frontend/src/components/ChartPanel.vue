be352a7abc7bff924121c9c0f9153d04e14770f1
O. Lewis Domingo <lewisbolaton@gmail.com>
2025-10-12 21:32:55 +0800

    Add chart for objectives stats

--- frontend/src/components/ChartPanel.vue
+++ frontend/src/components/ChartPanel.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="panel-container">
-    <!-- Conditionally render components based on selectedPlatform -->
-    <ChartCostDistribution v-if="!store.selectedPlatform" />
-    <ChartMeanCPU v-else />
+    <ChartCostDistribution v-if="showChart === 'ChartCostDistribution'" />
+    <ChartMeanCPU v-if="showChart === 'ChartMeanCPU'" />
+    <ChartObjectiveCPU v-if="showChart === 'ChartObjectiveCPU'" />
   </div>
 </template>
 
@@ -10,15 +10,54 @@
 import { store } from '../store.js';
 import ChartCostDistribution from './ChartCostDistribution.vue';
 import ChartMeanCPU from './ChartMeanCPU.vue';
+import ChartObjectiveCPU from './ChartObjectiveCPU.vue';
 
 export default {
   name: 'ChartPanel',
   components: { 
     ChartCostDistribution,
-    ChartMeanCPU
+    ChartMeanCPU,
+    ChartObjectiveCPU
   },
-  setup() {
-    return { store };
+  data() {
+    return {
+      showChart: 'ChartCostDistribution', // Default chart
+    };
+  },
+  computed: {
+    selectedPlatform() {
+      return store.selectedPlatform;
+    },
+    selectedObjective() {
+      return store.selectedObjective;
+    }
+  },
+  watch: {
+    selectedPlatform(newPlatform) {
+      this.updateChart(newPlatform, this.selectedObjective);
+    },
+    selectedObjective(newObjective) {
+      this.updateChart(this.selectedPlatform, newObjective);
+    }
+  },
+  mounted() {
+    // Initialize the chart when the component is mounted
+    this.updateChart(this.selectedPlatform, this.selectedObjective);
+  },
+  methods: {
+    // Update the chart based on the selected platform and objective
+    updateChart(platform, objective) {
+      // If both are selected, don't change the chart
+      if (platform && objective) return;
+
+      if (platform && !objective) {
+        this.showChart = 'ChartMeanCPU';
+      } else if (!platform && objective) {
+        this.showChart = 'ChartObjectiveCPU';
+      } else {
+        this.showChart = 'ChartCostDistribution';
+      }
+    }
   }
 }
 </script>
